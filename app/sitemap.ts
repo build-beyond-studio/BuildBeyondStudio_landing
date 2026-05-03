94648b6d7dc2b1c188c58b982a3927dc197ed37c
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2026-05-03 08:25:29 +0530

    added blogs for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -32,5 +32,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: 'https://buildbeyondstudio.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://buildbeyondstudio.com/blog/why-branding-critical-startup-growth',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://buildbeyondstudio.com/blog/high-converting-website-increases-revenue',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://buildbeyondstudio.com/blog/seo-strategies-modern-businesses',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://buildbeyondstudio.com/blog/how-marketing-agencies-offer-web-development-without-hiring-developers',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }
